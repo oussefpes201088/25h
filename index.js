0884412a9b879cb1ce5a5facba6e05059875577c
oussefpes201088 <youstha2@gmail.com>
2024-10-17 02:28:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ client.on('ready', async () => {
     .setName('lamine 3mk')
     .setDetails(`ادعوا ل امي بلرحمه`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/1293701323891408947/1295612198940446750/DiscordNitro.webp?ex=670f4863&is=670df6e3&hm=a5e416fa8f137b773aa1b7e6e44b55ab06794a7bcced0214318f67fc5c79df71&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1296209977274535946/1296253196024418334/DiscordNitro.webp?ex=67119d5d&is=67104bdd&hm=689119b2756f95886cbfc085546d68693e0a402a278dfb7e8c658b96abafec9e&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('Nitro') 
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1293701323891408947/1295611500106485832/Screenshot_-2.png?ex=670f47bc&is=670df63c&hm=20a00c0de8808e9ea3578a0fe8a3b570bb72f7e142cd2e63eec292362e610146&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1296209977274535946/1296253195743531008/Screenshot_-2.png?ex=67119d5d&is=67104bdd&hm=3a8d0af647d8fb14e5bb1075f79e60d4c9802ca12fe8b53282eda54009e70f41&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Small Text') 
-    .addButton('My Server', 'https://discord.gg/GseuvuA6');
+    .addButton('My Server', 'https://discord.gg/hypers');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
